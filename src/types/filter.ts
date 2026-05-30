5e2be1c19c0e809a4963dfeb233f655cb1a16b4d
Papichaya-Dev <lo0kpad@hotmail.com>
2026-05-30 14:41:06 +0700

    feat(utils): paginate helper for table pagination

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -2,3 +2,10 @@ import type { AllocationStatus, OrderPriority } from "./allocation";
 
 export type PriorityFilterType = OrderPriority | "ALL";
 export type StatusFilterType = AllocationStatus | "ALL";
+
+export interface PaginationResult<T> {
+	paginatedItems: T[];
+	totalPages: number;
+	totalItems: number;
+	currentPage: number;
+}
